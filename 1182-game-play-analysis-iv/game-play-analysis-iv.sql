68c71597caaf43fd99959a0164d56bf95cdbfb2a
Jaskaran Singh <jaskaran137dev@gmail.com>
2025-07-27 10:36:47 +0530

    Time: 591 ms (43.53%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -0,0 +1,17 @@
+WITH FirstLogin AS (
+    SELECT player_id, MIN(event_date) AS first_login
+    FROM Activity
+    GROUP BY player_id
+),
+NextDayLogin AS (
+    SELECT DISTINCT a.player_id
+    FROM Activity a
+    JOIN FirstLogin f
+      ON a.player_id = f.player_id
+     AND a.event_date = DATE_ADD(f.first_login, INTERVAL 1 DAY)
+)
+
+SELECT 
+    ROUND(COUNT(n.player_id) / COUNT(f.player_id), 2) AS fraction
+FROM FirstLogin f
+LEFT JOIN NextDayLogin n ON f.player_id = n.player_id;
