6fc1678ba54f9d3983dada06c835963d37c9310b
Jaskaran Singh <jaskaran137dev@gmail.com>
2025-07-27 23:06:50 +0530

    Time: 1 ms (33.40%) | Memory: 41.8 MB (41.19%) - LeetSync

--- 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.java
+++ 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.java
@@ -1,26 +1,23 @@
 class Solution {
     public int countHillValley(int[] nums) 
-    {
-       int count = 0;
-        int i = 1;
-
-        while (i < nums.length - 1) {
-            int prev = i - 1;
-            while (prev >= 0 && nums[prev] == nums[i]) prev--;
-
-            int next = i + 1;
-            while (next < nums.length && nums[next] == nums[i]) next++;
-
-            if (prev >= 0 && next < nums.length) {
-                if ((nums[i] > nums[prev] && nums[i] > nums[next]) ||
-                    (nums[i] < nums[prev] && nums[i] < nums[next])) {
-                    count++;
-                }
+    {List<Integer> simplified = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0 || nums[i] != nums[i - 1]) {
+                simplified.add(nums[i]);
             }
-
-            i = next;
         }
 
-        return count;
+        int terrainFeatures = 0;
+        for (int i = 1; i < simplified.size() - 1; i++) {
+            int left = simplified.get(i - 1);
+            int current = simplified.get(i);
+            int right = simplified.get(i + 1);
+
+            if ((current > left && current > right) || (current < left && current < right)) {
+                terrainFeatures++;
+            }
+        }
+
+        return terrainFeatures;
     }
 }
