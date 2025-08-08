4a5b3ad3e67c6212664812c06520581067c866a9
Jaskaran Singh <jaskaran137dev@gmail.com>
2025-08-08 09:05:25 +0530

    Time: 30 ms (33.71%) | Memory: 108 MB (61.58%) - LeetSync

--- 3643-zero-array-transformation-ii/zero-array-transformation-ii.java
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.java
@@ -2,23 +2,21 @@ class Solution {
     public int minZeroArray(int[] nums, int[][] queries) {
         int n = nums.length, m = queries.length;
         int l = 0, r = m, ans = -1;
-        long[] diff = new long[n + 1];
         while (l <= r) {
             int mid = (l + r) >>> 1;
-            if (check(nums, queries, mid, diff)) {
+            if (check(nums, queries, mid)) {
                 ans = mid;
                 r = mid - 1;
             } else l = mid + 1;
         }
         return ans == -1 ? -1 : ans;
     }
-    private boolean check(int[] nums, int[][] q, int k, long[] diff) {
+    private boolean check(int[] nums, int[][] q, int k) {
         int n = nums.length;
-        java.util.Arrays.fill(diff, 0, n + 1, 0L);
+        long[] diff = new long[n + 1];
         for (int i = 0; i < k; i++) {
             diff[q[i][0]] += q[i][2];
-            int r = q[i][1] + 1;
-            if (r <= n) diff[r] -= q[i][2];
+            diff[q[i][1] + 1] -= q[i][2];
         }
         long cur = 0;
         for (int i = 0; i < n; i++) {
