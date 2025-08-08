d7cbc5e4808e577c6ad68e1028d7902941e3ecd1
Jaskaran Singh <jaskaran137dev@gmail.com>
2025-08-08 09:06:33 +0530

    Time: 44 ms (9.98%) | Memory: 114.4 MB (48.59%) - LeetSync

--- 3643-zero-array-transformation-ii/zero-array-transformation-ii.java
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.java
@@ -2,24 +2,33 @@ class Solution {
     public int minZeroArray(int[] nums, int[][] queries) {
         int n = nums.length, m = queries.length;
         int l = 0, r = m, ans = -1;
+        long[] diff = new long[n + 1];
+        int[] seen = new int[n + 1];
+        int version = 0;
         while (l <= r) {
             int mid = (l + r) >>> 1;
-            if (check(nums, queries, mid)) {
+            version++;
+            if (check(nums, queries, mid, diff, seen, version)) {
                 ans = mid;
                 r = mid - 1;
             } else l = mid + 1;
         }
         return ans == -1 ? -1 : ans;
     }
-    private boolean check(int[] nums, int[][] q, int k) {
+    private boolean check(int[] nums, int[][] q, int k, long[] diff, int[] seen, int ver) {
         int n = nums.length;
-        long[] diff = new long[n + 1];
         for (int i = 0; i < k; i++) {
-            diff[q[i][0]] += q[i][2];
-            diff[q[i][1] + 1] -= q[i][2];
+            int L = q[i][0], R = q[i][1] + 1, v = q[i][2];
+            if (seen[L] != ver) { seen[L] = ver; diff[L] = 0; }
+            diff[L] += v;
+            if (R <= n) {
+                if (seen[R] != ver) { seen[R] = ver; diff[R] = 0; }
+                diff[R] -= v;
+            }
         }
         long cur = 0;
         for (int i = 0; i < n; i++) {
+            if (seen[i] != ver) { seen[i] = ver; diff[i] = 0; }
             cur += diff[i];
             if (nums[i] > cur) return false;
         }
