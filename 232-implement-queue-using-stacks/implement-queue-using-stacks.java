d102f42f499b71a3760e164c69996415faad4449
Jaskaran Singh <jaskaran137dev@gmail.com>
2025-11-12 06:42:46 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (7.48%) - LeetSync

---
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class MyQueue {
+    private final ArrayDeque<Integer> in = new ArrayDeque<>();
+    private final ArrayDeque<Integer> out = new ArrayDeque<>();
+
+    public MyQueue() {}
+
+    public void push(int x) {
+        in.push(x);
+    }
+    
+    public int pop() {
+        if (out.isEmpty()) while (!in.isEmpty()) out.push(in.pop());
+        return out.pop();
+    }
+    
+    public int peek() {
+        if (out.isEmpty()) while (!in.isEmpty()) out.push(in.pop());
+        return out.peek();
+    }
+    
+    public boolean empty() {
+        return in.isEmpty() && out.isEmpty();
+    }
+}
